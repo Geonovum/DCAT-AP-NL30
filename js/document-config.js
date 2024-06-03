f7b7776f6e6f63d06b3f1bf79c29c02cc1596c97
GN-BOT-WORKFLOW <99737370+GN-BOT-WORKFLOW@users.noreply.github.com>
2024-06-03 12:00:06 +0200

    Voeg issueBase toe

--- js/document-config.js
+++ js/document-config.js
@@ -35,7 +35,7 @@ var documentConfig =
   previousPublishDate: "2024-03-05",
   previousMaturity: "cv",*/
   edDraftURI: "https://geonovum.github.io/DCAT-AP-NL30/",
-  issueBase: "https://github.com/Geonovum/DCAT-AP-NL30/issues";
+  issueBase: "https://github.com/Geonovum/DCAT-AP-NL30/issues",
   authors: [
     {
       name: "Ine de Visser",
